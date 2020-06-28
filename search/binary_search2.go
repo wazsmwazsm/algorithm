f1d012a45b12c24763e9f962a8a474f8fb370efe
wazsmwazsm <942443360@qq.com>
2020-06-28 15:18:41 +0800

    update

--- search/binary_search2.go
+++ search/binary_search2.go
@@ -11,6 +11,10 @@ func main() {
 	fmt.Println(searchMax(a))
 	fmt.Println(searchMax(a2))
 	fmt.Println(searchMax(a3))
+
+	fmt.Println(searchMax2(a))
+	fmt.Println(searchMax2(a2))
+	fmt.Println(searchMax2(a3))
 }
 
 // 二分搜索的变种，查找一个先增序后降序的数组的最大值（转折点）
@@ -38,4 +42,25 @@ func searchMax(a []int) int {
 	}
 
 	return 0
+}
+
+// 第二种
+func searchMax2(a []int) int {
+	length := len(a)
+	if length == 0 {
+		return 0
+	}
+	left, right, mid := 0, length-1, 0
+
+	for left < right {
+		mid = left + (right-left)/2
+
+		if a[mid] <= a[mid+1] { // 小于右边的，那么在递增中，往右
+			left = mid + 1
+		} else { // 递减，往左
+			right = mid
+		}
+	}
+
+	return a[right] // 循环结束，此时 right 就是最先满足的点
 }
