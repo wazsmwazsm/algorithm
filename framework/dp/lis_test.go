929bdb155b4e738c991784a123bf3f4d787f5fbd
wazsmwazsm <942443360@qq.com>
2021-04-24 04:08:21 +0800

    update lis, add binary search

--- framework/dp/lis_test.go
+++ framework/dp/lis_test.go
@@ -5,7 +5,9 @@ import (
 )
 
 func TestLongestIncreasingSubsequence(t *testing.T) {
-
+	if 3 != longestIncreasingSubsequence([]int{4, 2, 3, 1, 5}) {
+		t.Error("lis err")
+	}
 	if 4 != longestIncreasingSubsequence([]int{10, 9, 2, 5, 3, 7, 101, 18}) {
 		t.Error("lis err")
 	}
@@ -19,21 +21,20 @@ func TestLongestIncreasingSubsequence(t *testing.T) {
 		t.Error("lis err")
 	}
 }
-
-// func TestLongestIncreasingSubsequence2(t *testing.T) {
-
-// 	if 4 != longestIncreasingSubsequence2([]int{10, 9, 2, 5, 3, 7, 101, 18}) {
-// 		t.Error("lis err")
-// 	}
-// 	if 5 != longestIncreasingSubsequence2([]int{10, 9, 2, 5, 3, 7, 101, 18, 20}) {
-// 		t.Error("lis err")
-// 	}
-
-// 	if 5 != longestIncreasingSubsequence2([]int{17, 8, 4, 3, 6, 11, 44, 15, 2, 79}) {
-// 		t.Error("lis err")
-// 	}
-
-// 	if 5 != longestIncreasingSubsequence2([]int{10, 2, 4, 6, 7, 8, 1, 5, 3}) {
-// 		t.Error("lis err")
-// 	}
-// }
+func TestLongestIncreasingSubsequence2(t *testing.T) {
+	if 3 != longestIncreasingSubsequence2([]int{4, 2, 3, 1, 5}) {
+		t.Error("lis err")
+	}
+	if 4 != longestIncreasingSubsequence2([]int{10, 9, 2, 5, 3, 7, 101, 18}) {
+		t.Error("lis err")
+	}
+	if 5 != longestIncreasingSubsequence2([]int{10, 9, 2, 5, 3, 7, 101, 18, 20}) {
+		t.Error("lis err")
+	}
+	if 5 != longestIncreasingSubsequence2([]int{17, 8, 4, 3, 6, 11, 44, 15, 2, 79}) {
+		t.Error("lis err")
+	}
+	if 5 != longestIncreasingSubsequence2([]int{10, 2, 4, 6, 7, 8, 1, 5, 3}) {
+		t.Error("lis err")
+	}
+}
